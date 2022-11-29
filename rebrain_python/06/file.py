f719290b296e6ce0bb6c50d07afcbcaeeeff3c87
ewill196 <inspect@inbox.ru>
2022-11-29 22:30:29 +0500

    new manners ro work with files

--- rebrain_python/06/file.py
+++ rebrain_python/06/file.py
@@ -1,8 +1,14 @@
-#with open('file01.txt', 'a') as f1, open('file02.txt') as f2:
+path = '/home/ewill/python/rebrain_python/06/'
+#with open(path+'file01.txt', 'a') as f1, open(path+'file02.txt') as f2:
 #    text = f2.read()
 #    f1.write('\n' + text)
 
-f = open('./file01.txt')
-text = f.read()
-print(text)
-f.close()
+#path = '/home/ewill/experements/files/'
+#f = open(path+'file01.txt')
+#text = f.read()
+#print(text)
+#f.close()
+
+w_file = open(path+'out.txt', 'w')
+w_file.writelines(['line1\n','line2\n', 'line3\n']) #список строк
+w_file.close()
